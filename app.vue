3165c2e16e193932c5deb6119f8e26e863309edc
wanyutang <wanyutang@gmail.com>
2023-06-28 14:22:25 +0800

    update

--- app.vue
+++ app.vue
@@ -3,5 +3,16 @@
     <NuxtLayout>
       <NuxtPage/>
     </NuxtLayout>
+    <!-- PWA component -->
+    <VitePwaManifest />
   </div>
 </template>
+<script>
+import { VitePwaManifest } from '@vite-pwa/nuxt'
+
+export default {
+  components: {
+    VitePwaManifest
+  }
+}
+</script>
